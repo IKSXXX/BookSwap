9b9132f82137218f88ebe38d2156976d444c874f
Anon <Anon@DESKTOP-DTLM9AJ>
2026-05-29 22:25:21 +0300

    модели

---
+++ BookSwap/BookSwap/Entities/Book.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookExchange.Web.Entities;
+
+public class Book : BaseEntity
+{
+    [Required, MaxLength(300)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required, MaxLength(200)]
+    public string Author { get; set; } = string.Empty;
+
+    [MaxLength(32)]
+    public string? ISBN { get; set; }
+
+    [MaxLength(4000)]
+    public string? Description { get; set; }
+
+    [MaxLength(500)]
+    public string? CoverImagePath { get; set; }
+
+    [MaxLength(100)]
+    public string Genre { get; set; } = "Другое";
+
+    public BookCondition Condition { get; set; } = BookCondition.Good;
+
+    public int? Year { get; set; }
+
+    [MaxLength(60)]
+    public string? Language { get; set; } = "Русский";
+
+    public bool IsAvailable { get; set; } = true;
+
+    public bool IsHidden { get; set; }
+}
---
+++ BookSwap/BookSwap/Entities/Enums.cs
@@ -0,0 +1,17 @@
+namespace BookExchange.Web.Entities;
+
+public enum BookCondition
+{
+    Excellent = 1,
+    Good = 2,
+    Acceptable = 3
+}
+
+public enum ExchangeStatus
+{
+    Pending = 0,
+    Accepted = 1,
+    Rejected = 2,
+    Completed = 3,
+    Cancelled = 4
+}
